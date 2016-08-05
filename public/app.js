9ffdb3a88458c5d321eeaf009b0d0b5c5d8d1996
kkindorf <kevin.kindorf@gmail.com>
2016-08-05 21:58:54 +0000

    pulling in data from guardian api

---
+++ public/app.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    var searchTerm = "";
+
+  $("form").submit(function(e){
+    e.preventDefault();
+    searchTerm = $("#term").val();
+    $('input').val('');
+    $(".results").html('');
+    $.getJSON("https://content.guardianapis.com/search?q="+searchTerm+"&order-by=relevance&api-key=b3970af8-30fc-4f88-a6d0-2e93e044a43c", function(data){
+      var resultsArr = data.response.results;
+      for(var i = 0; i <resultsArr.length; i++){
+          $(".results").append('<div class="panel panel-default"><div class="panel-body"><button                                    type="button" class="btn btn-default save">Save for Later</button>                                <p>'+resultsArr[i].sectionName+'</p><p><a                                                          href='+resultsArr[i].webUrl+'>'+resultsArr[i].webTitle+'</a></p>                                  </div></div>');
+      }
+  
+   })
+})
+});
