732c59baa8327076b430abb38e4681218d9733bb
kkindorf <kevin.kindorf@gmail.com>
2016-08-05 15:25:09 +0000

    one more push

--- test/test-server.js
+++ test/test-server.js
@@ -3,7 +3,6 @@ var chaiHttp = require('chai-http');
 var server = require('../server.js');
 var should = chai.should();
 var app = server.app;
-var storage = server.storage;
 chai.use(chaiHttp);
 
 
