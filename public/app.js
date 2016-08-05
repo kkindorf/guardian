2f314d525c517d9f549fe0ddc35b05c8f395f08a
kkindorf <kevin.kindorf@gmail.com>
2016-08-05 22:09:51 +0000

    added target=_blank attribute to links from guardian api

--- public/app.js
+++ public/app.js
@@ -9,7 +9,7 @@ $( document ).ready(function() {
     $.getJSON("https://content.guardianapis.com/search?q="+searchTerm+"&order-by=relevance&api-key=b3970af8-30fc-4f88-a6d0-2e93e044a43c", function(data){
       var resultsArr = data.response.results;
       for(var i = 0; i <resultsArr.length; i++){
-          $(".results").append('<div class="panel panel-default"><div class="panel-body"><button type="button" class="btn btn-default save">Save for Later</button><p>'+resultsArr[i].sectionName+'</p><p><a                                                          href='+resultsArr[i].webUrl+'target=_blank>'+resultsArr[i].webTitle+'</a></p>                                  </div></div>');
+          $(".results").append('<div class="panel panel-default"><div class="panel-body"><button type="button" class="btn btn-default save">Save for Later</button><p>'+resultsArr[i].sectionName+'</p><p><a                                                          href='+resultsArr[i].webUrl+'target="_blank">'+resultsArr[i].webTitle+'</a></p>                                  </div></div>');
       }
   
    })
