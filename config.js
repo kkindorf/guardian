b18073d1826415ffc9b3a9d1874a41767c28656d
kkindorf <kevin.kindorf@gmail.com>
2016-08-11 01:27:26 +0000

    added production database

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                        'mongodb://localhost/guardian' :
+                        'mongodb://kkindorf:Zooniebin9765@ds153785.mlab.com:53785/guardian-saver' :
                         'mongodb://localhost/guardian-dev');
 exports.PORT = process.env.PORT || 8080;
                          
