097dc768c4df6256e1ea6f8cd55eb35e69733482
kkindorf <kevin.kindorf@gmail.com>
2016-08-11 01:45:07 +0000

    added config file to master

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                       (process.env.NODE_ENV === 'production' ?
+                        'mongodb://kkindorf:Zooniebin9765@ds153785.mlab.com:53785/guardian-saver' :
+                        'mongodb://localhost/guardian-dev');
+exports.PORT = process.env.PORT || 8080;
